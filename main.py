cd5d9e7d78b1f3cf4809f0e0636189347348da9c
Samuel Kuld <61301218+SamuelKuld@users.noreply.github.com>
2020-11-10 17:58:54 -0800

    Third Commit. Added Plot and Grid Functions to allow us to construct a relative grid. Unsure on how I'll develop a way to view the grid using ASCII. In order to do that I think I'll need to develop an algorithm to fill in the blank tiles with spots that are empty. Or assume distance based on the previous X coordinate and Y coordinate. But for that I'd need to determine a way to sort these and I am not sure how to do that with both values in the same data structure. I suppose it would only take some math, But it would also require a pretty processor heavy algorithm to do so. If there was a way I could transfer that load onto GPU (Effectively what the entire game is based for) I imagine that would speed things up a bit. But for I'll just work on getting a functional algorithm.

--- main.py
+++ main.py
@@ -15,9 +15,53 @@ def wait_return():
     return input()
 
 
-def run():
+def menu():
+    print("Samuel's Game Of Life")
+    print("_____________________")
+    # TODO Update This Version Per Git Commit
+    print("Version 0.0.2")
     wait()
-    clear()
+
+
+def GetSimulationsList():
+    return os.listdir("simulations")
+
+
+def GetSimulations():
+    return "\n".join(GetSimulationsList())
+
+
+class Plot:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.type = "empty"
+
+
+class Grid:
+    def __init__(self):
+        self.plotList = []
+
+    def PlotInList(self, plot):
+        if plot in self.plotList:
+            return True
+        return False
+
+    def AddPlot(self, plot):
+        if self.PlotInList(plot):
+            raise Exception("Attempted to add a plot that was already in the plot list")
+        else:
+            self.plotList.append(plot)
+
+    def RemovePlot(self, plot):
+        if self.PlotInList(plot):
+            del self.plotList[self.plotList.index(plot)]
+        else:
+            raise Exception("Attempted to remove a plot that does not exist")
+
+
+def run():
+    menu()
 
 
 if __name__ == "__main__":
