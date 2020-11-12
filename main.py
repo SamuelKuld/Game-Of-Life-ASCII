d95540023bb66f85d6e57059bac2873826a53fc2
Samuel Kuld <61301218+SamuelKuld@users.noreply.github.com>
2020-11-11 21:02:30 -0800

    Fourth Commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+from msvcrt import getch
+import time
 
 
 def clear():
-    os.system("cls")
+    print("\n" * 10)
 
 
 def wait():
@@ -19,7 +21,7 @@ def menu():
     print("Samuel's Game Of Life")
     print("_____________________")
     # TODO Update This Version Per Git Commit
-    print("Version 0.0.2")
+    print("Version 0.0.3")
     wait()
 
 
@@ -31,37 +33,145 @@ def GetSimulations():
     return "\n".join(GetSimulationsList())
 
 
-class Plot:
+class XCoordinate:
     def __init__(self):
-        self.x = 0
-        self.y = 0
         self.type = "empty"
+
+
+class YCoordinate:
+    def __init__(self):
+        self.XCoordinates = []
+
+
+def getEmptyXRow(width):
+    return [XCoordinate() for i in range(width)]
+
+
+def getEmptyYColumns(height, width):
+    return [getEmptyXRow(width) for i in range(height)]
 
 
 class Grid:
     def __init__(self):
         self.plotList = []
+        self.plotListLength = len(self.plotList)
+        self.height = 0
+        self.width = 0
 
-    def PlotInList(self, plot):
-        if plot in self.plotList:
+    def setWidth(self, width):
+        self.width = width
+
+    def setHeight(self, height):
+        self.height = height
+
+    def constructGrid(self, height, width):
+        self.plotList.append(getEmptyYColumns(height, width))
+
+    def isPlotListEmpty(self):
+        if self.plotListLength > 0:
             return True
         return False
 
-    def AddPlot(self, plot):
-        if self.PlotInList(plot):
-            raise Exception("Attempted to add a plot that was already in the plot list")
+    def printGrid(self):
+        if self.isPlotListEmpty():
+            raise Exception("Attempted to print an empty grid.")
         else:
-            self.plotList.append(plot)
+            for y_coordinate in self.plotList:
+                for x_coordinate in y_coordinate:
+                    for unit in x_coordinate:
+                        print(unit.type, end=" ")
+                    print("")
 
-    def RemovePlot(self, plot):
-        if self.PlotInList(plot):
-            del self.plotList[self.plotList.index(plot)]
+
+def getWidthFixed():
+    clear()
+    print("That was not a proper value, please input the proper width you'd like the grid to be.")
+    return wait_return()
+
+
+def is_less_than_one(value):
+    if value < 1:
+        return True
+    return False
+
+
+def getWidth():
+    print("What would you like the width of the grid to be?")
+    try:
+        width = int(wait_return())
+        if is_less_than_one(width):
+            return False
+        return width
+    except ValueError:
+        return False
+
+
+def getHeight():
+    print("What would you like the height of the grid to be?")
+    try:
+        height = int(wait_return())
+        if is_less_than_one(height):
+            return False
         else:
-            raise Exception("Attempted to remove a plot that does not exist")
+            return height
+    except ValueError:
+        return False
+
+
+class Timer:
+
+    def __init__(self):
+        self.startTime = 0
+        self.end = 0
+        self.total = 0
+
+    def start(self):
+        self.startTime = time.time()
+
+    def stop(self):
+        self.total = time.time() - self.startTime
+
+    def readTime(self):
+        return self.total
+
+
+class TimerList:
+    def __init__(self):
+        self.timer = Timer()
+        self.timeTotals = []
+
+    def start(self):
+        self.timer.start()
+
+    def end(self):
+        self.timer.stop()
+
+    def iterateTimer(self):
+        self.timeTotals.append(self.timer.readTime())
+
+    def totalIterations(self):
+        total = 0
+        for i in self.timeTotals:
+            total += i
+        return total
+
+    def averageIterations(self):
+        return self.totalIterations() / len(self.timeTotals)
 
 
 def run():
-    menu()
+    timer = TimerList()
+    for i in range(200):
+        timer.start()
+        grid = Grid()
+
+        grid.constructGrid(20, 10)
+        grid.printGrid()
+        clear()
+        timer.end()
+        timer.iterateTimer()
+    print("Took - %s seconds" % (timer.averageIterations()))
+    wait()
 
 
 if __name__ == "__main__":
